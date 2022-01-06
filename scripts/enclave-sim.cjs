32cac0a56336646eed0e49d82c48b83b5582dfb7
Song Zhou <zs@ziorix.com>
2022-01-06 06:49:11 +0000

    enclave ci import bashrc

--- scripts/enclave-sim.cjs
+++ scripts/enclave-sim.cjs
@@ -5,10 +5,16 @@ const execSync = require('./execSync.cjs');
 const path = require('path');
 console.log('$ yarn enclave:sim', process.argv.slice(2).join(' '));
 
-function runInDocker() {
+function enclaveSIM() {
 
   const pathName = path.join(__dirname, "..");
-  execSync(`sudo docker run -v ${pathName}:/root/sgx -it baiduxlab/sgx-rust bash -c "cd /root/sgx/enclave && export SGX_MODE=SW && bash"`);
+  execSync(`sudo docker run -v ${pathName}:/root/sgx \
+    -it baiduxlab/sgx-rust \
+    bash -c "\
+      cd /root/sgx/enclave \
+      && export SGX_MODE=SW \
+      && bash"`
+  );
 }
 
-runInDocker()
+enclaveSIM()
