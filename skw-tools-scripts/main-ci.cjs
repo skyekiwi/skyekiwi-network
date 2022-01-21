8f9a9abc2f30d020e8bfbf58d2ead6a69e151d31
Song Zhou <zs@ziorix.com>
2022-01-21 15:32:10 +0800

    install target wasm32 for ci script

--- skw-tools-scripts/main-ci.cjs
+++ skw-tools-scripts/main-ci.cjs
@@ -5,6 +5,7 @@ const execSync = require('./execSync.cjs');
 console.log('$ yarn blockchain:ci', process.argv.slice(2).join(' '));
 
 function blockchainCi() {
+  execSync('rustup target add wasm32-unknown-unknown');
   execSync('SKIP_WASM_BUILD=1 cargo check --release');
   execSync('SKIP_WASM_BUILD=1 cargo check --features=runtime-benchmarks --release');
   execSync('SKIP_WASM_BUILD=1 cargo test --release');
